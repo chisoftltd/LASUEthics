3c6d3289dd853ce2a6e7a4859cfb1eea84a77e75
chisoftltd <chisoftltd@gmail.com>
2017-05-15 18:52:09 +0100

    Further Improvement

--- signinindex.php
+++ signinindex.php
@@ -80,7 +80,7 @@ include_once 'dbconnect.php';
         <p>You need to assess whether your project needs an ethical submission. This can be done by completing the RESSA
             form and based on the outcome decide whether an application is needed.</p>
         <p><a class="more"
-              href="http://www.LASU.ac.uk/download.cfm?downloadfile=5E84DCA0-2BEB-11E1-8D06000D609CAA9F&typename=dmFile&fieldname=filename">Researcher
+              href="http://www.lasu.edu.ng/images/bulletins/student_personal_ethics_statement_form.pdf">Researcher
                 and Supervisor Appraisal (RESSA) Form</a></p>
         <h3>Assessment of Research Ethics is very important expecially when the following groups are involved:</h3>
         <ul>
